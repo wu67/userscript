2a78693f288e82b5fa8f44932d26694691fa0b49
4thleaf <wk7113@gmail.com>
2016-11-17 13:59:35 +0800

    add @updateURL @downloadURL

---
+++ clean_robot_fans/clean_robot_fans.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        清理机器人粉丝
+// @namespace   wk
+// @description 自动拉黑关注你的贴吧机器人
+// @include     /^http:\/\/tieba\.baidu\.com\/i\/\d+\/fans.*/
+// @updateURL   https://github.com/4thleaf/userscript/raw/master/clean_robot_fans/clean_robot_fans.meta.js
+// @downloadURL https://github.com/4thleaf/userscript/raw/master/clean_robot_fans/clean_robot_fans.user.js
+// @author      与你偶遇的树下
+// @icon        http://himg.baidu.com/sys/portraitl/item/da35115e?t=1460692207
+// @license     MIT
+// @version     1.1
+// @grant       none
+// ==/UserScript==
