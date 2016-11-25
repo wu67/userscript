4d71637e2980b744213223ab983fc5cf9d096f2f
4thleaf <wk7113@gmail.com>
2016-11-25 17:33:13 +0800

    add auto_jd_deliver

--- README.md
+++ README.md
@@ -4,11 +4,18 @@
 - Chrome、Chromium 及相关的浏览器: [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
 - 其他浏览器请自行查找解决方案
 
-## clean_robot_fans.user.js
+## clean_robot_fans
 - 清理百度贴吧的机器人粉丝
 
-## auto_download_novel.user.js
+## auto_download_novel
 - 在小说家txt章节下载页，自动从第一话开始下载所有小说txt档
+- 里区(R18)的下载页也生效
 
+## auto_jd_deliver
+- 在京东 自动勾选"京东配送"
+- 尽可能将网页定位到 过滤选项上方的"**面包屑导航**"， 借此跳过导航条下方的**推广商品**，提高体验
+- **不**处理 商品排序规则 和 "只显示有货"选项
+  + 一来"综合排序"足够科学，没有综合排序的，JD自己就默认"销量排序"了
+  + 二则是并非每次逛JD都会买点什么，可以关注、到货再买
 
 ###### 以上脚本只是学习之余作为练习写的，并不保证完全可用；如有雷同，纯属巧合
---
+++ auto_jd_deliver/auto_jd_deliver.user.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name        自动勾选"京东配送"
+// @namespace   wk
+// @description 在京东自动勾选"京东配送"，并尽可能将网页定位到 过滤选项上方的"面包屑导航"
+// @include     /^https?:\/\/list\.jd\.com.+/
+// @include     /^https?:\/\/search\.jd\.com.+/
+// @updateURL   https://github.com/4thleaf/userscript/raw/master/auto_jd_deliver/auto_jd_deliver.meta.js
+// @downloadURL https://github.com/4thleaf/userscript/raw/master/auto_jd_deliver/auto_jd_deliver.user.js
+// @author      与你偶遇的树下
+// @icon        http://himg.baidu.com/sys/portraitl/item/da35115e?t=1460692207
+// @license     MIT
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+function addQuery(str, obj, position, bool) {
+
+    var a;
+
+    if (bool) {
+        // 直接给URL追加参数
+        if (!("delivery" in obj)) {
+            str += "&delivery=1";
+        }
+
+        if (!("wtype" in obj)) {
+            str += "&wtype=1";
+        }
+
+        str += "#J_crumbsBar";
+
+    } else {
+
+        // 传入的bool值为假时，原URL中有指定定位到某个HTML标签，此时应插入参数
+        a = str.slice(0, position);
+
+        if (!("delivery" in obj)) {
+            a += "&delivery=1";
+        }
+
+        if (!("wtype" in obj)) {
+            a += "&wtype=1";
+        }
+
+        a += str.slice(position, str.length);
+        str = a;
+    }
+
+    return str;
+
+}
+
+(function () {
+    var queryStr,
+        lastestURL,
+        keyWord = {},
+        jumpPosition;
+
+    queryStr = location.href.toString().split("?")[1].split("&");
+    lastestURL = location.href.toString();
+
+    for (var i = 0; i < queryStr.length; i++) {
+
+        var q = queryStr[i].split("=");
+        keyWord[q[0]] = q[1];
+    }
+    // console.log(keyWord);
+
+    jumpPosition = lastestURL.search(/#.*/);
+
+    if (jumpPosition === -1) {
+
+        // 若URL无定位到特定位置
+        // 直接追加参数
+        lastestURL = addQuery(lastestURL, keyWord, jumpPosition, true);
+    } else {
+
+        lastestURL = addQuery(lastestURL, keyWord, jumpPosition, false);
+    }
+
+    // console.log(location.href);
+    // console.log(lastestURL);
+    // console.log(jumpPosition);
+
+    if (location.href !== lastestURL) {
+        location.href = lastestURL;
+    }
+
+})();
