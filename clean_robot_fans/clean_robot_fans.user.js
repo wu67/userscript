4f44b5ac77f3aa22ad5f207e1eb3f90b9bd6e826
4thleaf <wk7113@gmail.com>
2017-01-25 16:15:01 +0800

    update domain
    目标 URL 更改；修正匹配网址正则

--- clean_robot_fans/clean_robot_fans.user.js
+++ clean_robot_fans/clean_robot_fans.user.js
@@ -2,13 +2,13 @@
 // @name        清理机器人粉丝
 // @namespace   wk
 // @description 自动拉黑关注你的贴吧机器人
-// @include     /^http:\/\/tieba\.baidu\.com\/i\/\d+\/fans.*/
+// @include     /^http:\/\/tieba\.baidu\.com\/i.*\/fans.*/
 // @updateURL   https://github.com/4thleaf/userscript/raw/master/clean_robot_fans/clean_robot_fans.meta.js
 // @downloadURL https://github.com/4thleaf/userscript/raw/master/clean_robot_fans/clean_robot_fans.user.js
 // @author      与你偶遇的树下
 // @icon        http://himg.baidu.com/sys/portraitl/item/da35115e?t=1460692207
 // @license     MIT
-// @version     1.1
+// @version     1.1.1
 // @grant       none
 // ==/UserScript==
 
